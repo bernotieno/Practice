a988a1e1a2870d5e266b1a0bb34ffdf5e41a3b90
bernaotieno <bernadokumu1@gmail.com>
2024-05-28 14:30:22 +0300

    mdofied so it can acommodate arguments thata are more than 10

--- utils/countparams.go
+++ utils/countparams.go
@@ -6,14 +6,43 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func Countparams() {
-	count := 0
+func Itoa(n int) string {
+	// If n is zero
+	if n == 0 {
+		return "0"
+	}
 
-	for i := 1; i < len(os.Args); i++ {
+	// Negative number
+	isNeg := false
+	if n < 0 {
+		isNeg = true
+		n = -n
+	}
+
+	// Convert to string
+	var result []byte
+	for n > 0 {
+		digit := n % 10
+		result = append([]byte{byte('0' + digit)}, result...)
+		n /= 10
+	}
+	if isNeg {
+		result = append([]byte{'-'}, result...)
+	}
+	return string(result)
+}
+
+func Countparams() {
+	var count int
+
+	arguments := os.Args[1:]
+
+	for range arguments {
 		count++
 	}
 
-	// Print the count
-	z01.PrintRune(rune(count + '0'))
+	for _, val := range Itoa(count) {
+		z01.PrintRune(val)
+	}
 	z01.PrintRune('\n')
 }
