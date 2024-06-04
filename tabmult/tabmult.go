dfab21fc1bb28cf01950905ad920a0ec44f3348d
bernaotieno <bernadokumu1@gmail.com>
2024-06-04 15:04:53 +0300

    tab mult broken down for easier understanding and step by step

--- tabmult/tabmult.go
+++ tabmult/tabmult.go
@@ -6,53 +6,61 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func Itoa(n int) string {
+	if n == 0 {
+		return "0"
+	}
+	isNeg := false
+	if n < 0 {
+		isNeg = true
+		n = -n
+	}
+	var number []rune
+	for n > 0 {
+		digit := n % 10
+		number = append([]rune{rune(digit + '0')}, number...)
+		n /= 10
+	}
+	if isNeg {
+		number = append([]rune{'-'}, number...)
+	}
+	return string(number)
+}
+
+func Atoi(str string) int {
+	num := 0
+	sign := 1
+
+	for i, char := range str {
+		if i == 0 && char == '-' {
+			sign = -1
+			continue
+		} else if i == 0 && char == '+' {
+			continue
+		}
+		if char < '0' || char > '9' {
+			return 0
+		}
+		num = num*10 + int(char-'0')
+	}
+	return num * sign
+}
+
+func Tabmult(s string) {
+	for i := 1; i <= 9; i++ {
+		STR := Itoa(i) + " " + "X" + " " + s + " " + "=" + " " + Itoa(i*Atoi(s))
+
+		for _, char := range STR {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
+
 func main() {
-	// Check if the number of command-line arguments is exactly 2
 	if len(os.Args) != 2 {
-		z01.PrintRune('\n') // Print a newline and exit if arguments are incorrect
 		return
 	}
-
-	stInt := os.Args[1] // Get the argument representing an integer
-	var number int      // Initialize a variable to store the integer value
-	var outPut []string // Initialize a slice to store the output strings
-
-	// Iterate over each character in the input string
-	for i, v := range stInt {
-		// Skip the first character if it is a '-' (negative sign)
-		if i == 0 && v == '-' {
-			return
-		}
-		// Convert the character to its integer value
-		n := int(v - 48)
-		// Build the integer by accumulating digits
-		number = ((number * 10) + n)
-	}
-
-	// Loop from 1 to 9 (inclusive)
-	for i := 1; i <= 9; i++ {
-		// Create a new string representing the current multiplier
-		newStr := string(rune(i + 48))
-		// Calculate the result of multiplication
-		res := (i * number)
-		resStr := "" // Initialize a string to store the result as a string
-
-		// Convert the result to a string by extracting digits
-		for res > 0 {
-			digit := res % 10                         // Extract the last digit
-			resStr = string(rune(digit+'0')) + resStr // Add the digit to the result string
-			res /= 10                                 // Move to the next digit
-		}
-
-		// Build the output string and append it to the output slice
-		outPut = append(outPut, newStr+" "+"x"+" "+stInt+" "+"="+" "+resStr)
-	}
-
-	// Print each string in the output slice
-	for i := 0; i < len(outPut); i++ {
-		for _, v := range outPut[i] {
-			z01.PrintRune(v) // Print each character of the string
-		}
-		z01.PrintRune('\n') // Print a newline after each output string
-	}
+	args := os.Args[1]
+	Tabmult(args)
 }
