47ccb8210a74d01d220b504787256e666a7f6e46
bernaotieno <bernadokumu1@gmail.com>
2024-06-14 09:21:06 +0300

    feat: add Itoa to help print coversion to string printing and add print it to print the 2D slice of integer

--- chunks/chunks.go
+++ chunks/chunks.go
@@ -1,14 +1,13 @@
 // The ChunkSlice function is designed to divide a given slice of integers into smaller chunks of a specified size and print these chunks.
 
-package main 
+package main
 
 import (
-	"fmt" 
+	"github.com/01-edu/z01"
 )
 
 func ChunkSlice(slice []int, chunkSize int) {
-	var chunks [][]int 
-
+	var chunks [][]int
 
 	for i := 0; i < len(slice); i += chunkSize {
 		end := i + chunkSize // Calculates the end index of the current chunk
@@ -20,7 +19,51 @@ func ChunkSlice(slice []int, chunkSize int) {
 		chunks = append(chunks, slice[i:end]) // Appends the current chunk to the chunks slice
 	}
 
-	fmt.Println(chunks) 
+	Printit(chunks)
+}
+
+func Printit(chunkies [][]int) {
+	z01.PrintRune('[')
+	for c, chunk := range chunkies {
+		z01.PrintRune('[')
+		for i, num := range chunk {
+			if i > 0 {
+				z01.PrintRune(' ')
+			}
+			numString := Itoa(num)
+			for _, r := range numString {
+				z01.PrintRune(r)
+			}
+		}
+		z01.PrintRune(']')
+		if c < len(chunkies)-1 {
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune(']')
+	z01.PrintRune('\n')
+}
+
+func Itoa(nb int) string {
+	if nb == 0 {
+		return "0"
+	}
+	isNeg := false
+	if nb < 0 {
+		isNeg = true
+		nb = -nb
+	}
+	var result []rune
+	for nb > 0 {
+		digit := nb % 10
+		result = append([]rune{rune(digit + '0')}, result...)
+		nb /= 10
+	}
+
+	if isNeg {
+		result = append([]rune{'-'}, result...)
+	}
+	return string(result)
 }
 
 func main() {
