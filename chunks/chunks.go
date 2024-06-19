e0793f5e5453feec481e816ccd9ed737013e39e5
bernaotieno <bernadokumu1@gmail.com>
2024-06-19 09:05:33 +0300

    handle if the chunksize is 0

--- chunks/chunks.go
+++ chunks/chunks.go
@@ -1,22 +1,23 @@
-// The ChunkSlice function is designed to divide a given slice of integers into smaller chunks of a specified size and print these chunks.
-
 package main
 
 import (
 	"github.com/01-edu/z01"
 )
 
+// The ChunkSlice function is designed to divide a given slice of integers into smaller chunks of a specified size and print these chunks.
 func ChunkSlice(slice []int, chunkSize int) {
+	if chunkSize <= 0 {
+		return
+	}
 	var chunks [][]int
 
 	for i := 0; i < len(slice); i += chunkSize {
-		end := i + chunkSize // Calculates the end index of the current chunk
-
-		if end > len(slice) { // Checks if the end index exceeds the length of the slice
-			end = len(slice) // Adjusts the end index to the length of the slice
+		end := i + chunkSize
+		if end > len(slice) {
+			end = len(slice)
 		}
 
-		chunks = append(chunks, slice[i:end]) // Appends the current chunk to the chunks slice
+		chunks = append(chunks, slice[i:end])
 	}
 
 	Printit(chunks)
