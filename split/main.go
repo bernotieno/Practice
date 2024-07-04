2fe48d244de06bac9d4db2b1e4c4e1dc116b666f
bernaotieno <bernadokumu1@gmail.com>
2024-07-04 15:50:51 +0300

    split

---
+++ split/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func Split(s, sep string) []string {
+	var result []string
+	start := 0
+	for i := 0; i < len(s); i++ {
+		if i+len(sep) <= len(s) && s[i:i+len(sep)] == sep {
+			result = append(result, s[start:i])
+			start = i + len(sep)
+			i += len(sep) - 1
+		}
+	}
+	result = append(result, s[start:])
+	return result
+}
+
+func main() {
+	s := "HelloHAhowHAareHAyou?"
+	fmt.Printf("%#v\n", Split(s, "HA"))
+}
