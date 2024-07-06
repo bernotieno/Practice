1c78e726866b9c2e47f7227b1150e5a0aa8e5a55
bernaotieno <bernadokumu1@gmail.com>
2024-07-06 14:10:05 +0300

    created a doop with atoi having to return bool and integer

--- doop/doop.go
+++ doop/doop.go
@@ -2,92 +2,97 @@ package main
 
 import (
 	"os"
-
-	"github.com/01-edu/z01"
 )
+
+func Itoa(n int) string {
+	if n == 0 {
+		return "0"
+	}
+	sign := ""
+	if n < 0 {
+		sign = "-"
+		n = -n
+	}
+	q := ""
+	for n > 0 {
+		digits := n % 10
+		q = string(rune('0'+digits)) + q
+		n /= 10
+	}
+	return sign + q
+}
+
+func Atoi(s string) (int, bool) {
+	sign := 1
+	q := 0
+
+	for i, v := range s {
+		if v == '-' && i == 0 {
+			sign = -1
+		} else if v == '+' && i == 0 {
+			sign = 1
+		} else if v >= '0' && v <= '9' {
+			q = q*10 + int(v-'0')
+		} else {
+			return 0, false
+		}
+	}
+	return sign * q, true
+}
+
+func PrintStr(s string) {
+	os.Stdout.WriteString(s)
+	os.Stdout.WriteString("\n")
+}
+
+func Calculation(a, operator, b string) string {
+	value1, ok1 := Atoi(a)
+	value2, ok2 := Atoi(b)
+
+	if !ok1 || !ok2 {
+		return ""
+	}
+
+	// Define max int64 value to handle large numbers
+	const maxInt64 = 9223372036854775807
+
+	if (value1 >= maxInt64 || value1 <= -maxInt64) || (value2 >= maxInt64 || value2 <= -maxInt64) {
+		return ""
+	}
+
+	q := 0
+	switch operator {
+	case "+":
+		q = value1 + value2
+	case "-":
+		q = value1 - value2
+	case "*":
+		q = value1 * value2
+	case "/":
+		if value2 == 0 {
+			return "No division by 0"
+		}
+		q = value1 / value2
+	case "%":
+		if value2 == 0 {
+			return "No modulo by 0"
+		}
+		q = value1 % value2
+	default:
+		return ""
+	}
+	return Itoa(q)
+}
 
 func main() {
 	if len(os.Args) != 4 {
 		return
 	}
+	value1, operator, value2 := os.Args[1], os.Args[2], os.Args[3]
 
-	value1 := Atoi(os.Args[1])
-	operator := os.Args[2]
-	value2 := Atoi(os.Args[3])
+	result := Calculation(value1, operator, value2)
 
-	if (value1 >= 9223372036854775807 || value1 <= -9223372036854775807) || (value2 >= 9223372036854775807 || value2 <= -9223372036854775807) {
-		return
+	if result != "" {
+		PrintStr(result)
 	}
-
-	var result int
-	switch operator {
-	case "+":
-		result = value1 + value2
-	case "-":
-		result = value1 - value2
-	case "*":
-		result = value1 * value2
-	case "/":
-		if value2 == 0 {
-			word := "No division by 0"
-			for _, c := range word {
-				z01.PrintRune(c)
-			}
-			z01.PrintRune('\n')
-			return
-		}
-		result = value1 / value2
-	case "%":
-		if value2 == 0 {
-			word := "No modulo by 0"
-			for _, c := range word {
-				z01.PrintRune(c)
-			}
-			z01.PrintRune('\n')
-			return
-		}
-		result = value1 % value2
-	default:
-		return
-	}
-	r := Itoa(int(result))
-	for _, c := range r {
-		z01.PrintRune(c)
-	}
-	z01.PrintRune('\n')
-}
-
-func Itoa(n int) string {
-	if n == 0 {
-		return "0"
-	} else if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-
-	var digits []rune
-	for n > 0 {
-		digit := n % 10
-		digits = append([]rune{rune('0' + digit)}, digits...)
-		n /= 10
-	}
-	return string(digits)
-}
-
-func Atoi(s string) int {
-	var number int
-	sign := 1
-
-	for idx, char := range s {
-		if char == '-' && idx == 0 {
-			sign = -1
-		} else if char == '+' && idx == 0 {
-			sign = 1
-		} else if char >= '0' && char <= '9' {
-			number = number*10 + int(char-'0')
-		} else {
-			return 0
-		}
-	}
-	return number * sign
 }
