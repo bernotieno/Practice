01fd81840442d502221b82fb28bdc56b13556e9e
bernotieno <bernadokumu1@gmail.com>
2024-08-22 10:45:45 +0300

    update of the function and addition of another that prints the unit32

--- option/main.go
+++ option/main.go
@@ -7,16 +7,24 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Println("Valid options are: -abcdefghijklmnopqrstuvwxyz")
+		fmt.Println("options: -abcdefghijklmnopqrstuvwxyz")
 		return
 	}
-	args := os.Args
+	args := os.Args[1:]
+	option(args)
+}
+
+func option(str []string) {
 	var optionsInt uint32 = 0
 
-	for _, arg := range args[1:] {
+	for _, arg := range str {
 		if len(arg) > 0 && arg[0] == '-' {
 			if len(arg) > 1 && contains(arg) {
-				fmt.Println("Valid options are: -abcdefghijklmnopqrstuvwxyz")
+				fmt.Println("options: -abcdefghijklmnopqrstuvwxyz")
+				return
+			}
+			if arg[0] == '-' && len(arg) == 1 {
+				fmt.Println("Invalid Option")
 				return
 			}
 			for _, ch := range arg[1:] {
@@ -27,7 +35,6 @@ func main() {
 					fmt.Println("Invalid Option")
 					return
 				}
-
 			}
 		} else {
 			fmt.Println("Invalid Option")
@@ -35,7 +42,7 @@ func main() {
 		}
 	}
 
-	bitString := fmt.Sprintf("%032b", optionsInt)
+	bitString := intToBinaryString(optionsInt, 32)
 
 	for i := 0; i < 32; i += 8 {
 		if (i + 8) < 32 {
@@ -46,6 +53,19 @@ func main() {
 	}
 	fmt.Println()
 }
+
+func intToBinaryString(n uint32, bitSize int) string {
+	binary := make([]byte, bitSize)
+	for i := 0; i < bitSize; i++ {
+		if n&(1<<(bitSize-i-1)) != 0 {
+			binary[i] = '1'
+		} else {
+			binary[i] = '0'
+		}
+	}
+	return string(binary)
+}
+
 func contains(s string) bool {
 	return s[:2] == "-h"
 }
