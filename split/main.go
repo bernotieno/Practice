c5e6900c614a30ae8b0bbb54eafa983ad87abf45
bernotieno <bernadokumu1@gmail.com>
2024-08-22 10:06:48 +0300

    update: thre split funtion

--- split/main.go
+++ split/main.go
@@ -5,14 +5,17 @@ import "fmt"
 func Split(s, sep string) []string {
 	var result []string
 	start := 0
-	for i := 0; i < len(s); i++ {
-		if i+len(sep) <= len(s) && s[i:i+len(sep)] == sep {
-			result = append(result, s[start:i])
-			start = i + len(sep)
-			i += len(sep) - 1
+	for i := 0; i+len(sep) <= len(s); i++ {
+		if s[i:i+len(sep)] == sep {
+			if start < i {
+				result = append(result, s[start:i])
+				start = i + len(sep)
+			}
 		}
 	}
-	result = append(result, s[start:])
+	if start < len(s) {
+		result = append(result, s[start:])
+	}
 	return result
 }
 
