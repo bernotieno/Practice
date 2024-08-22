e324cf467503c32ef4e6b58ac168585d47137fb5
bernotieno <bernadokumu1@gmail.com>
2024-08-22 09:33:20 +0300

    update: the slice function

--- slice/main.go
+++ slice/main.go
@@ -4,32 +4,39 @@ import "fmt"
 
 // Slice extracts a sub-slice from a given slice of strings based on the provided indices.
 func Slice(a []string, nbrs ...int) []string {
+	length := len(a)
+
 	if len(nbrs) == 0 {
-		return nil
+		return a
 	}
 
-	// Handle start and end indices
-	start, end := 0, len(a)
-
-	if len(nbrs) > 0 {
-		start = nbrs[0]
-	}
+	start := nbrs[0]
+	end := length
 
 	if len(nbrs) > 1 {
 		end = nbrs[1]
 	}
 
-	// Convert negative indices to positive equivalents
+	// Handle negative indices
 	if start < 0 {
-		start = len(a) + start
+		start = length + start
 	}
-
 	if end < 0 {
-		end = len(a) + end
+		end = length + end
 	}
 
-	// Ensure indices are within bounds
-	if start < 0 || start > len(a) || end < 0 || end > len(a) || start > end {
+	// Ensure start and end are within bounds
+	if start < 0 {
+		// start = 0
+		return []string{}
+	}
+	if end > length {
+		// end = length
+		return []string{}
+	}
+
+	// Return nil if start is greater than end or length
+	if start >= length || start >= end {
 		return nil
 	}
 
