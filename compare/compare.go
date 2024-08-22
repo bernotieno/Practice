c8a220b79d4d289aaec197b3b992d90595d7abbd
bernotieno <bernadokumu1@gmail.com>
2024-08-22 16:05:30 +0300

    update the function

--- compare/compare.go
+++ compare/compare.go
@@ -3,21 +3,13 @@ package main
 import "fmt"
 
 func Compare(a, b string) int {
-	for i := 0; i < len(a) && i < len(b); i++ {
-		if a[i] < b[i] {
-			return -1
-		} else if a[i] > b[i] {
-			return 1
-		}
-	}
-
-	if len(a) < len(b) {
+	if a < b {
 		return -1
-	} else if len(a) > len(b) {
+	} else if a > b {
 		return 1
+	} else {
+		return 0
 	}
-
-	return 0
 }
 
 func main() {
