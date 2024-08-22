a612844fc3c33bdbd0bc9fffce58a3e25391f7fd
bernotieno <bernadokumu1@gmail.com>
2024-08-22 09:30:30 +0300

    update: the revconcat function

--- revconcatalternate/main.go
+++ revconcatalternate/main.go
@@ -3,55 +3,24 @@ package main
 import "fmt"
 
 func RevConcatAlternate(slice1, slice2 []int) []int {
-	// Reverse the slices
-	reversedSlice1 := reverseSlice(slice1)
-	reversedSlice2 := reverseSlice(slice2)
+	len1 := len(slice1)
+	len2 := len(slice2)
+	maxLen := len1
+	if len2 > maxLen {
+		maxLen = len2
+	}
 
-	var result []int
-	i, j := 0, 0
-	len1, len2 := len(reversedSlice1), len(reversedSlice2)
+	result := make([]int, 0, len1+len2)
 
-	// Alternate adding elements from both slices, starting with the longer slice
-	for i < len1 && j < len2 {
-		if len1 > len2 {
-			result = append(result, reversedSlice1[i])
-			i++
-		} else if len2 > len1 {
-			result = append(result, reversedSlice2[j])
-			j++
-		} else {
-			result = append(result, reversedSlice1[i])
-			i++
-		}
+	for i := maxLen; i >= 0; i-- {
 		if i < len1 {
-			result = append(result, reversedSlice1[i])
-			i++
+			result = append(result, slice1[i])
 		}
-		if j < len2 {
-			result = append(result, reversedSlice2[j])
-			j++
+		if i < len2 {
+			result = append(result, slice2[i])
 		}
 	}
-
-	// Append remaining elements if any
-	for i < len1 {
-		result = append(result, reversedSlice1[i])
-		i++
-	}
-	for j < len2 {
-		result = append(result, reversedSlice2[j])
-		j++
-	}
-
 	return result
-}
-
-func reverseSlice(slice []int) []int {
-	reversed := make([]int, len(slice))
-	for i, v := range slice {
-		reversed[len(slice)-i-1] = v
-	}
-	return reversed
 }
 
 func main() {
